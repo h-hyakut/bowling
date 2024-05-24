6b6e811d20faf7a8d42cdb0779d33e2f7a6977f6
h-hyakut <hocha01n@gmail.com>
2024-05-24 17:10:20 +0900

    ファイル新規作成

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -14,6 +14,8 @@
 class Bowling
     def initialize 
         @total_score = 0
+        @score = []
+        @temp = []
     end
 
     def total_score
@@ -21,24 +23,49 @@ class Bowling
     end
 
     def add_score(pins)
-        @total_score += pins #total_score = total_score+ pins
+        # @total_score += pins #total_score = total_score+ pins
+        @temp << pins
+        if @temp.size == 2
+            @score << @temp
+            @temp = []
+        end
     end
-end
 
-#     @game = Bowling.new
-# #インスタンス変数に代入＝インスタンスを生成
+    def calc_score
+        @score.each.with_index(1) do |score, index| #index:スコアの番号
+            # index(1) =>本来は0,1,2...で数えるけど、1から始まるようにする❣
+            if spare?(score) && not_last_frame?(index)
+                @total_score += calc_space_bonus(index)
+                # if (score.inject(:+) == 10) && (index < 10)
+            #     @total_score += 10 + @score[index].first #[index:番号のみが入っている]
+            else
+                @total_score += score.inject(:+) #そのframeをそのまま合計に加算
+            end
+        end
+    end
 
-#     20.times do
-#     @game.add_score(1)
-#     end
+    private
+    #スペアかどうか判定する
+    def spare?(score)
+        score.inject(:+) == 10
+    end
 
-#     @game.total_score == 20    #testで書いた数値を用いて正常に動くかどうか
+    def not_last_frame?(index)  #index == num わかりやすい
+        index < 10
+    end
 
-# はい、正解です！`@game.add_score(1)` の呼び出しは、`Bowling` クラスのインスタンスである `@game` の `add_score` メソッドを呼び出しています。
-# 具体的には、次のように動作します：
-# 1. `Bowling.new` を呼び出すことで、Bowlingクラスの新しいインスタンス @gameが生成されます。
-# 2. `20.times do ... end` のループ内で、`@game.add_score(1)` を20回繰り返します。
-# 3. `add_score`メソッドは、`@total_score` に引数 `pins` を加えるためのものです。このメソッドは `Bowling` クラス内で定義されており、インスタンス変数 `@total_score` を操作します。
+    def calc_score_bonus(index)
+        10 + @score[index].first
+    end
+ end
 
-# つまり、`@game.add_score(1)` の呼び出しは、`Bowling` クラスのインスタンスである `@game` の `add_score` メソッドを呼び出し、その中で `@total_score` を更新します。
+
+
+
+
+
+
+
+
+
 
--- spec/lib/bowling_spec.rb
+++ spec/lib/bowling_spec.rb
@@ -1,7 +1,7 @@
 require "bowling"
 
 
-describe "ボウリングのスコア" do #メイン：ボウリングのスコア計算
+describe "ボウリングのスコア計算" do #メイン：ボウリングのスコア計算
     before do # describeのdoからendまで毎回実行される
         @game = Bowling.new
     end
@@ -69,6 +69,7 @@ describe "ボウリングのスコア" do #メイン：ボウリングのスコ�
                 @game.add_score(7)
                 #2
                 @game.add_score(4)
+
                 add_many_scores(15,0)
                 #
                 @game.add_score(3)
@@ -80,48 +81,17 @@ describe "ボウリングのスコア" do #メイン：ボウリングのスコ�
             end
         end
      end
-end
+ end
 
 
-private
-#複数回のスコア追加をまとめて実行する
+
+private #複数回のスコア追加をまとめて実行する
 def add_many_scores(count, pins)
     count.times do
         @game.add_score(pins)
     end
 end
 
+
 #Ubuntu $ rspec spec/lib/bowling_spec.rb =>テストの実行
 
-
-# 1. `require "bowling"`: テストで使用する`bowling`モジュールを読み込みます。
-
-# 2. `describe "ボウリングのスコア計算" do`: テストスイートのメインのブロックを開始します。このブロックは、ボウリングのスコア計算に関するテストを含みます。
-
-# 3. `describe "全体の合計" do`: テストスイート内のサブグループとして、ボウリングのスコア計算の全体の合計に関するテストを含むブロックを開始します。
-
-# 4. `context "全ての投球がガターだった場合" do`: テストケースのコンテキストを示します。このコンテキストでは、全ての投球がガターだった場合のテストを含みます。
-
-# 5. `it "0になること" do`: このブロック内のテストケースの説明を提供します。ここでは、全ての投球がガターだった場合のスコアが0になることを検証します。
-
-# 6. `@game = Bowling.new`: `Bowling`クラスの新しいインスタンスを作成し、`@game`インスタンス変数に代入します。
-
-# 7. `20.times do`: 20回の繰り返しを行います。
-
-# 8. `@game.add_score(0)`: `@game`に0を投球のスコアとして追加します。
-
-# 9. `expect(@game.total_score).to eq 0`: 期待される値として、`@game`の合計スコアが0であることを検証します。
-
-# 10. `context "全ての投球で1ピンずつ倒した場合" do`: もう1つのコンテキストを開始し、全ての投球で1ピンずつ倒した場合のテストを含みます。
-
-# 11. `it "20になること" do`: このブロック内のテストケースの説明を提供します。ここでは、全ての投球で1ピンずつ倒した場合のスコアが20になることを検証します。
-
-# 12. `@game = Bowling.new`: 同様に、`Bowling`クラスの新しいインスタンスを作成し、`@game`インスタンス変数に代入します。
-
-# 13. `20.times do`: 20回の繰り返しを行います。
-
-# 14. `@game.add_score(1)`: `@game`に1を投球のスコアとして追加します。
-
-# 15. `expect(@game.total_score).to eq 20`: 期待される値として、`@game`の合計スコアが20であることを検証します。
-
-# 16. 最後の`end`は、すべてのブロックの終了を示します。
