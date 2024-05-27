7b638b1f56f3d387c914925bc39d1ef747eb42ec
h-hyakut <hocha01n@gmail.com>
2024-05-27 09:46:18 +0900

    ファイル新規作成

--- spec/lib/bowling_spec.rb
+++ spec/lib/bowling_spec.rb
@@ -1,187 +1,163 @@
 require "bowling"
 
-# describe "ボウリングのスコア計算" do #メイン：ボウリングのスコア計算
-#     before do # describeのdoからendまで毎回実行される
-#         @game = Bowling.new
-#     end
-
-#     describe "全体の合計" do #サブ:スコア計算の全体の合計
-#         context "全ての投球がガターだった場合" do #
-#             it "0になること" do
-#                 add_many_scores(20,0)
-#                 # @game = Bowling.new #instance作成
-#                 # 20.times do
-#                 #     @game.add_score(0) #method
-#                 # end
-#                 @game.calc_score #合計を計算
-#                 expect(@game.total_score).to eq 0 #期待する実際の数値
-#             end
-#         end
-
-#         context "全ての投球で１ピンずつ倒した場合" do
-#             it "20になること" do
-#                 add_many_scores(20,1) #重複を減らす：再利用性を高める
-#                 # @game = Bowling.new    #@game:新規インスタンス
-#                 # 20.times do
-#                 #     @game.add_score(1)
-#                 # end
-#                 @game.calc_score
-#                 expect(@game.total_score).to eq 20 #期待する実際の数値
-#             end 
-#         end
-
-#         context "スペアを取った場合" do
-#             it "スペアボーナスが加算されること" do
-#                 @game.add_score(3)
-#                 @game.add_score(7)
-
-#                 @game.add_score(4)
-                
-#                 add_many_scores(17,0)
-
-#                 @game.calc_score
-
-#                 expect(@game.total_score).to eq 18
-#             end
-#         end
-
-#         context "フレーム違いでスペアになるようなスコアだった場合" do
-#             it "スペアボーナスが加算されないこと" do
-#                 #1 
-#                 @game.add_score(3)
-#                 @game.add_score(5)
-#                 #2
-#                 @game.add_score(5)
-#                 @game.add_score(4)
-
-#                 add_many_scores(16,0)
-                
-#                 @game.calc_score
-#                 expect(@game.total_score).to eq 17
-#             end
-#         end
-
-#         context "最終フレームでスペアを取った場合" do
-#             it "スペアボーナスが加算されないこと" do
-#                 #1
-#                 @game.add_score(3)
-#                 @game.add_score(7)
-#                 #2
-#                 @game.add_score(4)
-
-#                 add_many_scores(15,0)
-#                 #
-#                 @game.add_score(3)
-#                 @game.add_score(7)
-
-#                 @game.calc_score
-
-#                 expect(@game.total_score).to eq 28
-#             end
-#         end
-#      end
-#  end
-
-
-
-# private #複数回のスコア追加をまとめて実行する
-# def add_many_scores(count, pins)
-#     count.times do
-#         @game.add_score(pins)
-#     end
-# end
-
-
-# #Ubuntu $ rspec spec/lib/bowling_spec.rb =>テストの実行
-
-# bowling_spec.rb
-
-describe "ボウリングのスコア計算" do
-    # インスタンスの生成を共通化
-    before do
-      @game = Bowling.new
+describe "ボウリングのスコア計算" do #メイン：ボウリングのスコア計算
+    before do # describeのdoからendまで毎回実行される
+        @game = Bowling.new
     end
-  
-    describe "全体の合計" do
-      context "全ての投球がガターだった場合" do
-        it "0になること" do
-          add_many_scores(20, 0)
-          # 合計を計算
-          @game.calc_score
-          expect(@game.total_score).to eq 0
+
+    describe "全体の合計" do #サブ:スコア計算の全体の合計
+        context "全ての投球がガターだった場合" do #
+            it "0になること" do
+                add_many_scores(20,0)
+                # @game = Bowling.new #instance作成
+                # 20.times do
+                #     @game.add_score(0) #method
+                # end
+                @game.calc_score #合計を計算
+                expect(@game.total_score).to eq 0 #期待する実際の数値
+            end
         end
-      end
-  
-      context "全ての投球で1ピンずつ倒した場合" do
-        it "20になること" do
-          add_many_scores(20, 1)
-          # 合計を計算
-          @game.calc_score
-          expect(@game.total_score).to eq 20
+
+        context "全ての投球で１ピンずつ倒した場合" do
+            it "20になること" do
+                add_many_scores(20,1) #重複を減らす：再利用性を高める
+                # @game = Bowling.new    #@game:新規インスタンス
+                # 20.times do
+                #     @game.add_score(1)
+                # end
+                @game.calc_score
+                expect(@game.total_score).to eq 20 #期待する実際の数値
+            end 
         end
-      end
-  
-      context "スペアを取った場合" do
-        it "スペアボーナスが加算されること" do
-          # 第一フレームで3点, 7点のスペア
-          @game.add_score(3)
-          @game.add_score(7)
-          # 第二フレームの一投目で4点
-          @game.add_score(4)
-          # 以降は全てガター
-          add_many_scores(17, 0)
-          # 合計を計算
-          @game.calc_score
-          # 期待する合計　※()内はボーナス点
-          # 3 + 7 + 4 + (4) = 18
-          expect(@game.total_score).to eq 18
+
+        context "スペアを取った場合" do
+            it "スペアボーナスが加算されること" do
+                @game.add_score(3)
+                @game.add_score(7)
+
+                @game.add_score(4)
+                
+                add_many_scores(17,0)
+
+                @game.calc_score
+
+                expect(@game.total_score).to eq 18
+            end
         end
-      end
-  
-      context "フレーム違いでスペアになるようなスコアだった場合" do
-        it "スペアボーナスが加算されないこと" do
-          # 第一フレームで3点, 5点
-          @game.add_score(3)
-          @game.add_score(5)
-          # 第二フレームで5点, 4点
-          @game.add_score(5)
-          @game.add_score(4)
-          # 以降は全てガター
-          add_many_scores(16, 0)
-          # 合計を計算
-          @game.calc_score
-          # 期待する合計
-          # 3 + 5 + 5 + 4 = 17
-          expect(@game.total_score).to eq 17
+
+        context "フレーム違いでスペアになるようなスコアだった場合" do
+            it "スペアボーナスが加算されないこと" do
+                #1 
+                @game.add_score(3)
+                @game.add_score(5)
+                #2
+                @game.add_score(5)
+                @game.add_score(4)
+
+                add_many_scores(16,0)
+                
+                @game.calc_score
+                expect(@game.total_score).to eq 17
+            end
         end
-      end
-  
-      context "最終フレームでスペアを取った場合" do
-        it "スペアボーナスが加算されないこと" do
-          # 第一フレームで3点, 7点のスペア
-          @game.add_score(3)
-          @game.add_score(7)
-          # 第二フレームの一投目で4点
-          @game.add_score(4)
-          # 15投は全てガター
-          add_many_scores(15, 0)
-          # 最終フレームで3点, 7点のスペア
-          @game.add_score(3)
-          @game.add_score(7)
-          # 合計を計算
-          @game.calc_score
-          # 期待する合計　※()内はボーナス点
-          # 3 + 7 + 4 + (4) + 3 + 7 = 28
-          expect(@game.total_score).to eq 28
+
+        context "最終フレームでスペアを取った場合" do
+            it "スペアボーナスが加算されないこと" do
+                #1
+                @game.add_score(3)
+                @game.add_score(7)
+                #2
+                @game.add_score(4)
+
+                add_many_scores(15,0)
+                #
+                @game.add_score(3)
+                @game.add_score(7)
+
+                @game.calc_score
+                expect(@game.total_score).to eq 28
+            end
         end
-      end
+
+        context "ストライクを取った場合" do
+            it "ストライクボーナスが加算されること" do
+                @game.add_score(10)
+
+                @game.add_score(5) 
+                @game. add_score(4)
+
+                add_many_scores(16,0)
+
+                @game.calc_score
+                #10+ \ +5+(5) + 4+(4)
+                expect(@game.total_score).to eq 28
+            end
+        end
+
+        context "ダブルを取った場合" do
+            it "それぞれのストライクボーナスが加算されること" do
+                @game.add_score(10)
+
+                @game.add_score(10)
+
+                @game.add_score(5) 
+                @game. add_score(4)
+                
+                add_many_scores(14,0)
+
+                @game.calc_score
+                #10+ 10+(10) +5+(5) +(5) + 4+(4)
+                expect(@game.total_score).to eq 53
+            end
+        end
+
+        context "ターキーを取った場合" do
+            it "それぞれのストライクボーナスが加算されること" do
+                @game.add_score(10)
+
+                @game.add_score(10)
+
+                @game.add_score(10)
+
+                @game.add_score(5) 
+                @game. add_score(4)
+                
+                add_many_scores(12,0)
+
+                @game.calc_score
+                # 10 + 10 +(10) + 10+(10)+(10) +5+(5)+(5) + 4+(4) = 83
+                expect(@game.total_score).to eq 83
+            end
+        end
+
+        context "最終フレームでストライクを取った場合" do
+            it "ストライクボーナスが加算されないこと" do
+                @game.add_score(10)
+
+                @game.add_score(5) 
+                @game. add_score(4)
+                
+                add_many_scores(14,0)
+
+                @game.add_score(10)
+
+                @game.calc_score
+                ## 10 + 10 + (10) + 10 + (10 + 10) + 5 +(5 + 5) + 4 + (4) = 83
+                expect(@game.total_score).to eq 38
+            end
+        end
     end
-  end
-  
-  private
-  # 複数回のスコア追加をまとめて実行する
-  def add_many_scores(count, pins)
+end
+
+         
+
+private #複数回のスコア追加をまとめて実行する
+def add_many_scores(count, pins)
     count.times do
-      @game.add_score(pins)
+        @game.add_score(pins)
     end
-  end
+end
+
+
+#Ubuntu $ rspec spec/lib/bowling_spec.rb =>テストの実行
+
